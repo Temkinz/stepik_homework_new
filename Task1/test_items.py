4d40d4a81df8cca52bde238cacbdc4a0dedd771f
Temkinz <50596199+Temkinz@users.noreply.github.com>
2020-09-24 18:40:11 +0300

    corrected typo

--- Task1/test_items.py
+++ Task1/test_items.py
@@ -1,13 +1,12 @@
 
 import time
 
-
 locator_add_to_cart = "button.btn.btn-lg.btn-primary.btn-add-to-basket"
+language_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_page_language(browser):
 	#Action
-	language_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 	browser.get(language_link)
 	time.sleep(30)
 	button_exist = len(browser.find_elements_by_css_selector(locator_add_to_cart))
